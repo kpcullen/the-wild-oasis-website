aa1ed3b3912e82f8f551a4b166060a1d8d7fe71e
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-06-27 16:23:30 +0200

    refactor code on filter buttons

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -30,7 +30,7 @@ export default function Page({ searchParams }) {
       <div className="flex justify-end mb-8">
         <Filter />
       </div>
-      <Suspense fallback={<Spinner />}>
+      <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
       </Suspense>
     </div>
