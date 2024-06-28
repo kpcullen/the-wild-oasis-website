62df7223a923abaf14e6c173453807cf9aa55aa3
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-06-28 15:23:53 +0200

    begins implementing authorization

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,5 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
-
 import "@/app/_styles/globals.css";
-
+import { ReservationProvider } from "@/app/_components/ReservationContext";
 import { Josefin_Sans } from "next/font/google";
 import Header from "./_components/Header";
 
@@ -30,7 +27,9 @@ export default function RootLayout({ children }) {
       >
         <Header />
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
