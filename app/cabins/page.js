b1c5b63417bce56d986cd6c34ccd8b3168ef5fbb
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-07-01 18:37:38 +0200

    implements reservation date range picker

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
 import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
 export const revalidate = 3600;
 
@@ -32,6 +33,7 @@ export default function Page({ searchParams }) {
       </div>
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
